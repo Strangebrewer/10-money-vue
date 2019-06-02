f0893dafd5b3eef1389b6fd3e7ac6132198432a1
Strangebrewer <BKAShambala@gmail.com>
2019-06-02 04:24:17 -0600

    Vue app skeleton setup

---
+++ client/src/components/Landing.vue
@@ -0,0 +1,37 @@
+<template>
+	<b-container class="landing">
+		<b-row>
+			<b-col>
+				<b-jumbotron :header="msg" :lead="lead">
+					<p>For more information visit website</p>
+					<b-button variant="primary" @click="$bvModal.show('modal-scoped')">More Info</b-button>
+					<b-modal id="modal-scoped">
+                  <template slot="modal-title">
+                     This isn't a website. Narf!
+                  </template>
+               </b-modal>
+				</b-jumbotron>
+			</b-col>
+		</b-row>
+		<b-row>
+			<b-col cols="6">
+				<b-card></b-card>
+			</b-col>
+		</b-row>
+	</b-container>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			msg: "Landing Page",
+			lead: "It all starts here."
+		};
+	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Landing from '@/components/Landing';
+import VueLinks from '@/components/VueLinks'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Landing',
+      component: Landing
+    },
+    {
+      path: '/vue-links',
+      name: 'Vue links',
+      component: VueLinks
     }
   ]
 })
