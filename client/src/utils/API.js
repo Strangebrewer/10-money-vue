aefcc3c180f4bb0c975ebfa915a0ca310d9e617a
Strangebrewer <BKAShambala@gmail.com>
2019-09-19 01:11:17 -0600

    setup to work with digital ocean api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,10 +2,9 @@ import axios from 'axios'
 
 function API() {
    return axios.create({
-      // baseURL: 'https://bkashambhala.com',
-      // baseURL: 'https://167.71.116.62',
-      baseURL: 'http://localhost:3001', // use this at work
-      // baseURL: 'http://localhost:8080', // use this at home
+      baseURL: 'https://bkashambhala.com', // use this to connect to the api on digitalocean
+      // baseURL: 'http://localhost:3000', // use this at work
+      // baseURL: 'http://localhost:3001', // use this at home
       headers: {
          Authorization: `Bearer ${localStorage.getItem('token')}`
       }
